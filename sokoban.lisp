b15d8263c020802db13239f20d734e279cda9493
Strigoides <Strigoides@gmail.com>
2013-02-12 09:05:33 +1300

    Level loading

--- sokoban.lisp
+++ sokoban.lisp
@@ -1,3 +1,20 @@
-(defpackage sokoban)
+(defpackage sokoban
+  (:use :common-lisp))
 
 (in-package :sokoban)
+
+(defun level-from-string (string)
+  (declare (optimize (debug 3)))
+  (let ((level '(())))
+    (loop for char across string
+          if (char= char #\Newline)
+            do (push () level)
+          else
+            do (push (ecase char
+                       (#\# 'wall)
+                       (#\. 'storage)
+                       (#\Space 'empty)
+                       (#\@ 'man)
+                       (#\o 'crate))
+                     (car level)))
+    (reverse (mapcar #'reverse level))))
